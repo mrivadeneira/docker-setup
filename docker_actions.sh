cb6f95e42e60b40d0fe23cf0ffea98cd35da474b
mrivadeneira <maxirivadeneira92@gmail.com>
2022-08-18 23:09:09 -0300

    update docker actions

--- docker_actions.sh
+++ docker_actions.sh
@@ -2,4 +2,6 @@ docker pull <dockerhub image repo>
 docker run <image name>
 docker pause <container id>
 docker unpause <container id>
+docker restart <container id>
 docker stop <container id>
+docker kill <container id>
